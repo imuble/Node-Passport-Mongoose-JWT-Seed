abcfbb7ac1403e1fc33976d6210762f203d57092
lorang92 <Steffen.ekeberg@gmail.com>
2016-06-18 11:36:05 +0200

    Added missing constant files.

--- server/routes/login/signToken.js
+++ server/routes/login/signToken.js
@@ -1,15 +1,26 @@
 const jwt = require('jsonwebtoken');
 var serverConfig = require('../../config/server.config');
+var tokenConfig = require('../../config/token.config');
+var tokenConstants = require('./token.constants');
 
 module.exports = function (req, res, next) {
 	var token = jwt.sign({
 		id: req.user.id,
-		userVersion: req.user.userVersion,
-	}, serverConfig.SECRET, {expiresIn: serverConfig.TOKEN.EXPIRATION_TIME}
-);
+		userVersion: req.user.__v,
+		privileges: req.user.privileges,
+		type: tokenConstants.TYPES.ACCESS_TOKEN
+	}, serverConfig.SECRET, {expiresIn: tokenConfig.ACCESS_TOKEN_EXPIRATIONTIME});
+
+	var refreshToken = jwt.sign({
+		id: req.user.id,
+		userVersion: req.user.__v,
+		privileges: req.user.privileges,
+		type: tokenConstants.TYPES.REFRESH_TOKEN
+	}, serverConfig.SECRET, {expiresIn: tokenConfig.REFRESH_TOKEN_EXPIRATONTIME});
 
 	var data = {
 		token: token,
+		refreshToken: refreshToken
 	};
 
 	req.data = data;
--- server/routes/login/verifyToken.js
+++ server/routes/login/verifyToken.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken');
 var serverConfig = require('../../config/server.config');
+var tokenConstants = require('./token.constants');
 
 module.exports = function (req, res, next) {
 	if (!req.header('Authorization')) return res.status(401).json({message: 'Missing token'});
 	jwt.verify(req.header('Authorization'), serverConfig.SECRET, function (err, verified) {
 		if (err) return res.status(401).json({message: err.message});
+		if (verified.type === tokenConstants.TYPES.REFRESH_TOKEN) return res.status(401).json({message: 'A refresh token can not be used as an access token.'});
 		else {
 			req.verified = verified;
 			next();
